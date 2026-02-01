013083693a1d8dd1152198600e25799d049ab229
Lain Soykaf <lain@lain.com>
2026-02-01 17:08:02 +0400

    build: add test filter option
    - Allow running a subset of Zig tests via -Dtest-filter=...

--- build.zig
+++ build.zig
@@ -13,6 +13,7 @@ fn addLibraryPathIfExists(module: *std.Build.Module, path: []const u8) void {
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const test_filter = b.option([]const u8, "test-filter", "Only run tests matching this substring");
 
     const c_flags = &[_][]const u8{
         "-std=c99",
@@ -75,10 +76,12 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the server");
     run_step.dependOn(&run_cmd.step);
 
-    const mod_tests = b.addTest(.{ .root_module = mod });
+    const filters = if (test_filter) |f| &.{f} else &.{};
+
+    const mod_tests = b.addTest(.{ .root_module = mod, .filters = filters });
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    const exe_tests = b.addTest(.{ .root_module = exe.root_module });
+    const exe_tests = b.addTest(.{ .root_module = exe.root_module, .filters = filters });
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
